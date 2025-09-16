b4f16fe4d6997faf924b6d9708938cd9c525cd40
Claude Warren <claude.warren@aiven.io>
2025-09-16 14:25:16 +0100

    initial working copy

--- tools/src/main/java/io/aiven/site/tools/ReleaseNotesGenerator.java
+++ tools/src/main/java/io/aiven/site/tools/ReleaseNotesGenerator.java
@@ -1,49 +1,53 @@
 package io.aiven.site.tools;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.filefilter.WildcardFileFilter;
-import org.semver4j.Semver;
-
 import java.io.File;
 import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
+import org.semver4j.Semver;
 
 public class ReleaseNotesGenerator {
-    private final Map<Semver, File> fileMap;
+  private final Map<Semver, File> fileMap;
 
-    public ReleaseNotesGenerator(File dir) {
-        FileFilter fileFilter = WildcardFileFilter.builder().setWildcards("*.txt").get();
-        File[] files = dir.listFiles(fileFilter);
-        fileMap = new TreeMap<>();
-        if (files != null) {
-            for (File f : files) {
-                Semver version = Semver.parse(f.getName().substring(0, f.getName().lastIndexOf('.')));
-                fileMap.put(version, f);
-            }
-        }
-    }
+  public ReleaseNotesGenerator() {
+    fileMap = new TreeMap<>((o1, o2) -> o2.compareTo(o1));
+  }
 
-    public final String entry(String version) throws FileNotFoundException {
-        Semver semver = Semver.parse(version);
-        File f = fileMap.get(semver);
-        if (f == null) {
-            throw new FileNotFoundException(version + ".txt");
+  public final void configure(final String directory) {
+    File dir = new File(directory);
+    FileFilter fileFilter = WildcardFileFilter.builder().setWildcards("*.txt").get();
+    File[] files = dir.listFiles(fileFilter);
+    fileMap.clear();
+    if (files != null) {
+      for (File f : files) {
+        Semver version = Semver.parse(f.getName().substring(0, f.getName().lastIndexOf('.')));
+        if (version != null) {
+          fileMap.put(version, f);
         }
-        return String.join( "\n", IOUtils.readLines(new FileInputStream(f), Charset.defaultCharset()));
+      }
     }
+  }
 
-    public final String allEntries() throws FileNotFoundException {
-        StringBuilder sb = new StringBuilder();
-        for (File f : fileMap.values()) {
-            String contents = String.join( "\n", IOUtils.readLines(new FileInputStream(f), Charset.defaultCharset()));
-            sb.append(contents).append('\n');
-        }
-        return sb.toString();
+  public final String entry(final String version) throws FileNotFoundException {
+    Semver semver = Semver.parse(version);
+    File f = fileMap.get(semver);
+    if (f == null) {
+      throw new FileNotFoundException(version + ".txt");
     }
+    return String.join("\n", IOUtils.readLines(new FileInputStream(f), Charset.defaultCharset()));
+  }
+
+  public final String allEntries() throws FileNotFoundException {
+    StringBuilder sb = new StringBuilder();
+    for (File f : fileMap.values()) {
+      String contents = String.join("\n", IOUtils.readLines(new FileInputStream(f), Charset.defaultCharset()));
+      sb.append(contents).append('\n');
+    }
+    return sb.toString();
+  }
 }
--- tools/src/test/java/io/aiven/site/tools/ReleaseNotesGeneratorTest.java
+++ tools/src/test/java/io/aiven/site/tools/ReleaseNotesGeneratorTest.java
@@ -1,4 +1,40 @@
 package io.aiven.site.tools;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ReleaseNotesGeneratorTest {
+
+    private static ReleaseNotesGenerator releaseNotesGenerator = new ReleaseNotesGenerator();
+
+
+    @BeforeEach
+    public void setup() {
+        releaseNotesGenerator.configure("src/test/resources/ReleaseNotesTest");
+    }
+    @Test
+    void entryTest() throws FileNotFoundException {
+        String actual = releaseNotesGenerator.entry("1.1.1");
+        assertEquals("File 1.1.1", actual);
+    }
+
+    @Test
+    void allEntriesTest() throws FileNotFoundException {
+        String expected =
+                "File 2.0.0\n" +
+                "File 1.2.0\n" +
+                "File 1.1.2\n" +
+                "File 1.1.1\n" +
+                "File 1.1.1-stuff\n" +
+                "File 1.1.0\n" +
+                "File 1.0.0\n";
+        String actual = releaseNotesGenerator.allEntries();
+        assertEquals(expected, actual);
+    }
 }
